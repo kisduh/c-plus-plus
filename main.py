14398ee801a56b526e81c2a8af27c96e1514c829
kisduh <realanaana@gmail.com>
2024-12-03 11:52:38 +0000

    This is a fun madlib program to make your own stories.

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello khizrann")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+famous = input("famous person:")
+madlib= f"Computer programming is so {adj} ! It makes me excited all the time because \
+I love to {verb1}. Stay hydrated and productive and be like {famous}"
+print(madlib)
